4a73b90bcf298592690c9e765c5725130b55711d
sumonkaysar <msk.sumon.kaysar@gmail.com>
2024-06-11 15:57:57 +0600

    added see more products functionalities

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// mobile menu bar toggling
+
 document.getElementById("menu-bar-btn").addEventListener("click", () => {
     const menuBar = document.getElementsByClassName("menu-container")[0];
     console.log();
@@ -5,8 +7,23 @@ document.getElementById("menu-bar-btn").addEventListener("click", () => {
         return menuBar.classList.remove("shown");
     }
     menuBar.classList.add("shown");
-})
+});
 
+// see more popular items functionalities
+const seeMoreBtn = document.getElementById("see-more-btn");
+seeMoreBtn.addEventListener("click", () => {
+    const seeMoreContainer = document.getElementById("more-items-container");
+    if ([...seeMoreContainer.classList].includes('hidden')) {
+        seeMoreBtn.querySelector(".more-less").innerHTML = "Less"
+        seeMoreBtn.querySelector("i").style.transform = "rotate(180deg)"
+        return seeMoreContainer.classList.remove("hidden");
+    }
+    seeMoreBtn.querySelector(".more-less").innerHTML = "More"
+    seeMoreBtn.querySelector("i").style.transform = "rotate(0deg)"
+    seeMoreContainer.classList.add("hidden");
+});
+
+// subscription functionalities
 document.getElementById("subscribe-form").addEventListener("submit", e => {
     e.preventDefault();
     const email = e.target.email.value
@@ -17,4 +34,4 @@ document.getElementById("subscribe-form").addEventListener("submit", e => {
     } else {
         result.innerHTML = "<span class='error'>Email can't be blank</span>";
     }
-})
+});
